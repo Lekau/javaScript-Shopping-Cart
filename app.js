406901669432f42d40efa20518a84eb5336289c1
lekau <lekau9222@gmail.com>
2021-11-19 22:28:43 +0200

    rendering different products from an array

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 /* app.js */
+const productsEl = document.querySelector(".products");
+
+function renderProducts(){
+    products.forEach((product) => {
+        productsEl.innerHTML += `
+            <div class="item">
+                <div class="item-container">
+                    <div class="item-img">
+                        <img src="${product.imgSrc}"t-shirt 1">
+                    </div>
+                    <div class="desc">
+                        <h2>${product.name}</h2>
+                        <h2><small>$</small>${product.price}</h2>
+                        <p>
+                            ${product.description}
+                        </p>
+                    </div>
+                    <div class="add-to-wishlist">
+                        <img src="./icons/heart.png" alt="add to wish list">
+                    </div>
+                    <div class="add-to-cart">
+                        <img src="./icons/bag-plus.png" alt="add to cart">
+                    </div>
+                </div>
+            </div>
+        `
+    });
+}
+renderProducts()
